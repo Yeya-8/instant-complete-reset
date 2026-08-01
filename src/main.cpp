5fb651db4ae9bd770a17a5b05be736838e465abc
Yeya_29 <yehyakun2017@gmail.com>
2026-08-01 23:56:50 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,12 @@ using namespace geode::prelude;
 
 class $modify(PlayLayer) {
     void keyDown(cocos2d::enumKeyCodes key) {
-        if (key == cocos2d::enumKeyCodes::KEY_R && this->m_hasCompletedLevel) {
-            this->m_hasCompletedLevel = false;
+        if (key == cocos2d::KEY_R && m_hasCompletedLevel) {
+            m_hasCompletedLevel = false;
             this->resetLevel();
             return;
         }
-        
+
         PlayLayer::keyDown(key);
     }
-};	 * return type `void` and taking a `CCObject*`.
-	*/
-	void onMyButton(CCObject*) {
-		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
-	}
 };
