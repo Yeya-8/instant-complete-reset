91efd55890b43957037a85d53956704816938777
Yeya_29 <yehyakun2017@gmail.com>
2026-08-02 12:26:36 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,28 @@
 
 using namespace geode::prelude;
 
+class $modify(MyPlayLayer, PlayLayer) {
+    struct Fields {
+        bool isCompleting = false;
+    };
+
+    void levelComplete() {
+        m_fields->isCompleting = true;
+        PlayLayer::levelComplete();
+    }
+};
+
 class $modify(InstantResetDispatcher, CCKeyboardDispatcher) {
     bool dispatchKeyboardMSG(cocos2d::enumKeyCodes key, bool isKeyDown, bool isKeyRepeat, double time) {
         if (isKeyDown && key == cocos2d::KEY_R) {
             if (auto pl = PlayLayer::get()) {
-                if (pl->m_hasCompletedLevel) {
-                    log::info("=== Scene dump ===");
-                    auto scene = cocos2d::CCDirector::sharedDirector()->getRunningScene();
-                    if (auto children = scene->getChildren()) {
-                        for (int i = 0; i < children->count(); i++) {
-                            auto node = static_cast<cocos2d::CCNode*>(children->objectAtIndex(i));
-                            log::info("Scene child {}: {} (zOrder {}, visible {})",
-                                i, typeid(*node).name(), node->getZOrder(), node->isVisible());
-                        }
-                    }
-                    log::info("PlayLayer children count: {}", pl->getChildrenCount());
-                    log::info("=== End dump ===");
+                auto modPl = static_cast<MyPlayLayer*>(pl);
+                if (modPl->m_fields->isCompleting) {
+                    modPl->m_fields->isCompleting = false;
+                    pl->m_hasCompletedLevel = false;
+                    log::info("Completion detected early, resetting now");
+                    pl->resetLevel();
+                    return true;
                 }
             }
         }
