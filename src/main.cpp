36a0a723d5cfc1173ef655845a620404ff694258
Yeya_29 <yehyakun2017@gmail.com>
2026-08-02 13:56:51 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,23 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
-#include <Geode/modify/CCKeyboardDispatcher.hpp>
+#include <Windows.h>
 
 using namespace geode::prelude;
 
 class $modify(MyPlayLayer, PlayLayer) {
     struct Fields {
-        bool lastCompleted = false;
-        bool lastEndChecked = false;
+        bool wasRDown = false;
     };
 
     void checkForEnd() {
         PlayLayer::checkForEnd();
 
-        if (m_hasCompletedLevel != m_fields->lastCompleted) {
-            m_fields->lastCompleted = m_hasCompletedLevel;
-            log::info("m_hasCompletedLevel changed to {}", m_hasCompletedLevel);
+        bool rDown = (GetAsyncKeyState('R') & 0x8000) != 0;
+        if (rDown && !m_fields->wasRDown && m_endChecked) {
+            log::info("R detected via raw poll during end sequence, resetting");
+            m_hasCompletedLevel = false;
+            this->resetLevel();
         }
-        if (m_endChecked != m_fields->lastEndChecked) {
-            m_fields->lastEndChecked = m_endChecked;
-            log::info("m_endChecked changed to {}", m_endChecked);
-        }
-    }
-};
-
-class $modify(InstantResetDispatcher, CCKeyboardDispatcher) {
-    bool dispatchKeyboardMSG(cocos2d::enumKeyCodes key, bool isKeyDown, bool isKeyRepeat, double time) {
-        if (isKeyDown && key == cocos2d::KEY_R) {
-            log::info("R pressed");
-        }
-        return CCKeyboardDispatcher::dispatchKeyboardMSG(key, isKeyDown, isKeyRepeat, time);
+        m_fields->wasRDown = rDown;
     }
 };
